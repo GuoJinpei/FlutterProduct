331176b308b74b299e927d646ccd42c2917800d3
guojp <guojp@bsoft.com.cn>
2020-04-01 17:14:52 +0800

    案例2动态案例

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,69 @@
 import 'package:flutter/material.dart';
 
-//void main() {
-//  MyApp();
-//}
-main() => (runApp(MyApp()));
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyApp',
+      title: "MyApp",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("MyApp"),
-          ),
-          body: TextContent()
+        appBar: AppBar(
+          title: Text("MyApp"),
+        ),
+        body: ContentWidget(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            print("AddBtnClick");
+          },
+        ),
       ),
     );
   }
 }
 
-class TextContent extends StatelessWidget {
+class ContentWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return ContentWidgetState();
+  }
+}
+
+class ContentWidgetState extends State<ContentWidget> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "MyApp",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            color: Colors.orange,
-            fontSize: 30
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                setState(() {
+                  counter++;
+                });
+                print("$counter");
+              },
+              child: Text("计数+1"),
+            ),
+            RaisedButton(
+              onPressed: (){
+                setState(() {
+                  counter--;
+                });
+                print("$counter");
+              },
+              child: Text("计数-1"),
+            )
+          ],
         ),
-      ),
+        Text("当前计数$counter",style: TextStyle(fontSize: 25),)
+      ],
     );
   }
 }
